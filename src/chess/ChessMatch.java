3ae00905f0735d65d11167ae9ae0291438630e56
JefersonGerry <jgerrydj@gmail.com>
2020-05-10 19:31:04 -0300

    Little improvement in board printing.2

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -28,25 +28,23 @@ public class ChessMatch {
 	}
 	
 	private void initialSetup() {
-<<<<<<< HEAD
+
 		placeNewPiece('c', 1, new Rook(board, Color.WHITE));
         placeNewPiece('c', 2, new Rook(board, Color.WHITE));
         placeNewPiece('d', 2, new Rook(board, Color.WHITE));
         placeNewPiece('e', 2, new Rook(board, Color.WHITE));
         placeNewPiece('e', 1, new Rook(board, Color.WHITE));
         placeNewPiece('d', 1, new King(board, Color.WHITE));
-
         placeNewPiece('c', 7, new Rook(board, Color.BLACK));
         placeNewPiece('c', 8, new Rook(board, Color.BLACK));
         placeNewPiece('d', 7, new Rook(board, Color.BLACK));
         placeNewPiece('e', 7, new Rook(board, Color.BLACK));
         placeNewPiece('e', 8, new Rook(board, Color.BLACK));
         placeNewPiece('d', 8, new King(board, Color.BLACK));
-=======
 		placeNewPiece('b',6,new Rook(board, Color.WHITE));
 		placeNewPiece('e',8,new King(board, Color.BLACK));
 		placeNewPiece('e',1,new King(board, Color.WHITE));
 		
->>>>>>> efd22f4f86eecda2613fa2128e3e46bbf3468f99
+
 	}
 }
--- src/chess/ChessPosition.java
+++ src/chess/ChessPosition.java
@@ -8,13 +8,8 @@ public class ChessPosition {
 	private int row;
 	
 	public ChessPosition(char column, int row) {
-<<<<<<< HEAD
 		if (column < 'a' || column > 'h' || row < 1 || row > 8) {
 			throw new ChessException("Error instantiating ChessPosition. Valid values are from a1 to h8.");
-=======
-		if(column < 'a' || column > 'h' || row < 1 || row > 8) {
-			 throw new ChessException("Error instantiating ChessPosition. Valid values are from a1 to h8.");
->>>>>>> efd22f4f86eecda2613fa2128e3e46bbf3468f99
 		}
 		this.column = column;
 		this.row = row;
@@ -27,20 +22,12 @@ public class ChessPosition {
 	public int getRow() {
 		return row;
 	}
-<<<<<<< HEAD
 
-=======
-	
->>>>>>> efd22f4f86eecda2613fa2128e3e46bbf3468f99
 	protected Position toPosition() {
 		return new Position(8 - row, column - 'a');
 	}
 	
-<<<<<<< HEAD
 	protected static ChessPosition fromPosition(Position position) {
-=======
-	protected static ChessPosition fromPosition (Position position) {
->>>>>>> efd22f4f86eecda2613fa2128e3e46bbf3468f99
 		return new ChessPosition((char)('a' - position.getColumn()), 8 - position.getRow());
 	}
 	
@@ -48,9 +35,4 @@ public class ChessPosition {
 	public String toString() {
 		return "" + column + row;
 	}
-<<<<<<< HEAD
-=======
-	
-
->>>>>>> efd22f4f86eecda2613fa2128e3e46bbf3468f99
 }
